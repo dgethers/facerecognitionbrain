8311c23b777e8b56a606d56d72376e833bb75084
David Gethers <dgethers.jr@gmail.com>
2020-08-21 15:28:08 -0400

    Add ImageLinkForm component code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
-import Logo from './components/Logo/Logo';
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/*
       <ImageLinkForm />
+      {/*
       <FaceRecongition />*/}
     </div>
   );
