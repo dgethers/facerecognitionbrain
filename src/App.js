52c635a0c35b5274f05cd0fca67adfc6b7de36ab
David Gethers <dgethers.jr@gmail.com>
2020-08-21 15:08:48 -0400

    Add logo component code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
+import Logo from './components/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
+      <Logo />
+      {/*
       <ImageLinkForm />
       <FaceRecongition />*/}
     </div>
