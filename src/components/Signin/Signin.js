49faa071a587907fa53de6610c6d2a97421798e8
David Gethers <dgethers.jr@gmail.com>
2020-08-31 16:12:33 -0400

    Hardcode Heroku url for debugging purposes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch(`${process.env.BACKEND_HOST}/signin`, {
+    fetch("http://obscure-sands-82425.herokuapp.com/signin", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
